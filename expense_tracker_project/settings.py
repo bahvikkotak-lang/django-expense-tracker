28be1ab5ab41441c6184f69643a6eebcebb80cf0
Bhavik Kotak <bahvikkotak@gmail.com>
2026-02-20 20:39:18 +0530

    Production settings update

--- expense_tracker_project/settings.py
+++ expense_tracker_project/settings.py
@@ -4,9 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-1234567890'
 
-DEBUG = True
+DEBUG = False   
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
